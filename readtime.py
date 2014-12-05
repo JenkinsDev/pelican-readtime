cd762c514cc53a0a2258e1a3d800ea2e7a7b7e9b
David Jenkins <david.nicholas.jenkins@gmail.com>
2014-12-05 17:06:15 -0500

    Initial commit for my pelican read time plugin

---
+++ readtime.py
@@ -0,0 +1,87 @@
+from pelican import signals
+
+
+# We make AVERAGE_READING_WPM a float here so we don't have to make a float()
+# call later in the code base.  We do this because we need to get the amount of
+# time in fractional minutes and then convert that to total seconds.
+AVERAGE_READING_WPM = 275.0
+SECONDS_IN_MINUTE = 60
+
+
+def read_time(content):
+    """ Core function used to generate the read_time for content. Readtime is
+    algorithmically computed based on Medium's readtime functionality.
+    Read: https://medium.com/the-story/read-time-and-you-bc2048ab620c
+
+    Parameters:
+        :param content: Sub-Instance of pelican.content.Content
+
+    Returns:
+        None
+    """
+    if content_type_supported(content):
+        # We get the content's text, split it at the spaces and check the
+        # length of the provided array to get a good estimation on the amount
+        # of words in the content.
+        words = len(content.content.split())
+        read_time_seconds = round((words / AVERAGE_READING_WPM) * 60, 2)
+        minutes, seconds = get_time_from_seconds(read_time_seconds)
+        minutes_str = pluralize(minutes, "Minute", "Minutes")
+        seconds_str = pluralize(seconds, "Second", "Seconds")
+        content.readtime = "{} and {}".format(minutes_str, seconds_str)
+
+
+def pluralize(measure, singular, plural):
+    if measure == 1:
+        return "{} {}".format(measure, singular)
+    else:
+        return "{} {}".format(measure, plural)
+
+
+def get_time_from_seconds(read_time_seconds):
+    """ Returns, in a tuple, the amount of minutes and seconds based on the
+    provided seconds.  87 seconds would return (1, 27); 1 minute and 27 seconds.
+
+    Parameters:
+        :param read_time_seconds: The amount of seconds to retrieve time from.
+
+    Returns:
+        Tuple containing minutes and seconds
+    """
+    minutes = int(read_time_seconds / SECONDS_IN_MINUTE)
+    seconds = int(read_time_seconds % SECONDS_IN_MINUTE)
+    return minutes, seconds
+
+
+def content_type_supported(content):
+    """ Returns an answer to whether the content instance supplied is supported
+    by the current configuration.
+
+    Parameters:
+        :param content: Sub-Instance of pelican.content.Content
+
+    Returns:
+        None
+    """
+    if "READTIME_CONTENT_SUPPORT" in content.settings:
+        content_support = content.settings["READTIME_CONTENT_SUPPORT"]
+    else:
+        content_support = ["Article"]
+    return class_name(content) in content_support
+
+
+def class_name(obj):
+    """ A form of python reflection, returns the human readable, string formatted,
+    version of a class's name.
+
+    Parameters:
+        :param obj: Any object.
+
+    Returns:
+        A human readable string version of the supplied object's class name.
+    """
+    return obj.__class__.__name__
+
+
+def register():
+    signals.content_object_init.connect(read_time)
---
+++ tests.py
@@ -0,0 +1,49 @@
+import unittest
+from readtime import get_time_from_seconds, content_type_supported, pluralize
+
+
+class TestReadTime(unittest.TestCase):
+
+    SINGULAR_SECONDS = "Second"
+    PLURAL_SECONDS = "Seconds"
+
+    def test_get_time_from_seconds_returns_correct_amount_of_time(self):
+        minutes, seconds = get_time_from_seconds(87)
+        self.assertEquals(minutes, 1)
+        self.assertEquals(seconds, 27)
+        minutes, seconds = get_time_from_seconds(120)
+        self.assertEquals(minutes, 2)
+        self.assertEquals(seconds, 0)
+
+    def test_content_type_supported(self):
+        content = Content(content_support=["Content"])
+        self.assertTrue(content_type_supported(content))
+
+        content = Content(content_support=["NoArticle", "Page"])
+        self.assertFalse(content_type_supported(content))
+
+    def test_pluralize_minutes(self):
+        singular = "Minute"
+        plural = "Minutes"
+        minute = 1
+        minutes = 20
+        self.assertEquals("1 Minute", pluralize(minute, singular, plural))
+        self.assertEquals("20 Minutes", pluralize(minutes, singular, plural))
+
+    def test_pluralize_seconds(self):
+        singular = "Second"
+        plural = "Seconds"
+        second = 1
+        seconds = 20
+        self.assertEquals("1 Second", pluralize(second, singular, plural))
+        self.assertEquals("20 Seconds", pluralize(seconds, singular, plural))
+
+
+class Content():
+
+    def __init__(self, content_support):
+        self.settings = { "READTIME_CONTENT_SUPPORT": content_support }
+
+
+if __name__ == "__main__":
+    unittest.main()
